e6c5f8907ffbdbd50e6868b8e56d64d2c61b7fee
Ahmed-Mustafa132 <123089914+Ahmed-Mustafa132@users.noreply.github.com>
2023-07-05 11:48:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+
 document.addEventListener("keydown", event => {
     if(event.key==="ArrowLeft"){moveLeft();}
     if(event.key==="ArrowRight"){moveRight();}
   });
   var character = document.getElementById("character");
+
+
+
+//   =================================moveLeft=======================
+
+
+
   function moveLeft(){
       let left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
       left -= 100;
@@ -10,6 +18,10 @@ document.addEventListener("keydown", event => {
           character.style.left = left + "px";
       }
   }
+
+//   ==============moveRight====================
+
+
   function moveRight(){
       let left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
       left += 100;
@@ -17,14 +29,20 @@ document.addEventListener("keydown", event => {
           character.style.left = left + "px";
       }
   }
+
+
+//   =================== block game ========================= 
   var block = document.getElementById("block");
   var counter = 0;
-  block.addEventListener('animationiteration',  function hi() {
+  block.addEventListener('animationiteration',  function () {
       var random = Math.floor(Math.random() * 3);
       left = random * 100;
       block.style.left = left + "px";
       counter++;
   });
+
+
+//   ==============Game Over =========================
   setInterval(function(){
       var characterLeft = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
       var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
@@ -32,7 +50,10 @@ document.addEventListener("keydown", event => {
       if(characterLeft==blockLeft && blockTop<500 && blockTop>300){
           alert("Game over. Score: " + counter);
           block.style.animation = "none";
+          counter = 0
       }
+      document.getElementById("score").innerHTML = `${counter}`
+     
   },1);
   
   
@@ -41,3 +62,8 @@ document.addEventListener("keydown", event => {
   document.getElementById("right").addEventListener("touchstart", moveRight);
   document.getElementById("left").addEventListener("touchstart", moveLeft);
   
+// =============================play=========================
+
+function play(){
+    block.style.animation = ' slide 1s infinite linear'
+}
